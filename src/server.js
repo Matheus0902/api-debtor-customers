86715817cc2c1402dad7705e02577064747888a6
Matheus Antônio <matheus090298@hotmail.com>
2025-03-06 21:12:07 -0300

    Updated CORS

--- src/server.js
+++ src/server.js
@@ -12,11 +12,7 @@ migrationsRun()
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-  origin: ['http://localhost:5173', 'debtor-customer.netlify.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}))
+app.use(cors())
 
 app.use(routes)
 
