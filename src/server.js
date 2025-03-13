005e5a5ef121565f03bcedc7f8b5ec20b947ea98
Matheus Antônio <matheus090298@hotmail.com>
2025-03-13 20:02:41 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,8 +11,28 @@ const  routes = require("./routes")
 migrationsRun()
 
 const app = express()
+
+// Configuração CORS para permitir múltiplas origens
+const allowedOrigins = [
+  'https://debtor-customer.netlify.app', 
+  'http://localhost:5173',               // Front-end local (para desenvolvimento)
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Se não houver origem (requests do Postman ou servidores), libera
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
+app.use(cors(corsOptions)) 
+
 app.use(express.json())
-app.use(cors())
 
 app.use(routes)
 
