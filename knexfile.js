284ccc27576a086ce1a2adbd9c6eac3070517bf0
Matheus Antônio <matheus090298@hotmail.com>
2025-03-14 20:16:16 -0300

    update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: process.env.DATABASE_PATH || path.resolve(__dirname, '..', 'src', 'database', 'database.db')
     },
     
     pool: {
