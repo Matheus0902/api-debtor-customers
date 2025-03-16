a5a0d7f6b2e3ee3d5f8cc01d2116d38b76520c6f
Matheus Antônio <matheus090298@hotmail.com>
2025-03-16 10:22:36 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,21 +5,17 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: process.env.DATABASE_PATH || path.resolve(__dirname, '..', 'src', 'database', 'database.db')
-    },
-    
-    pool: {
-      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
 
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
-    }, 
-    
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')  // Caminho das migrações
+    },
+
     useNullAsDefault: true
   },
 
-  production: {
+  /*production: {
     client: 'sqlite3',
     connection: {
       filename: process.env.DATABASE_PATH || '/mnt/data/database.db'  // Caminho específico no Render
@@ -34,5 +30,5 @@ module.exports = {
     },
 
     useNullAsDefault: true
-  }
+  }*/
 }
